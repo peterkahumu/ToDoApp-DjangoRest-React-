951d470c4be5caa0b664cd9f960419b4228e4ac8
peterkahumu <muhumukip@gmail.com>
2025-03-25 17:47:05 +0300

    Add ToDo model and admin configuration

--- ToDoBackend/backend/settings.py
+++ ToDoBackend/backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # third-party apps
+    'rest_framework',
     
     # custom apps
     'todo',
--- ToDoBackend/todo/admin.py
+++ ToDoBackend/todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ToDo
 
 # Register your models here.
+@admin.register(ToDo)
+class ToDoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'memo', 'completed', 'user']
+    search_fields = ['title', 'memo']
+    list_filter = ['completed']
+    ordering = ['created_at']
+    
