5bae430b8b0007bfe271b51b5606a20d1e144457
peterkahumu <muhumukip@gmail.com>
2025-03-25 17:32:59 +0300

    Add initial Todo app structure and configuration

--- ToDoBackend/backend/settings.py
+++ ToDoBackend/backend/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third-party apps
+    
+    # custom apps
+    'todo',
 ]
 
 MIDDLEWARE = [
